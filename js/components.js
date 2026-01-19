05012f1aed56a778108c405bcd1eb361ef42fc7f
Shivam Gupta <shivam.s.gupta1211@gmail.com>
2026-01-19 13:10:24 +0530

    feat: introduce dynamic header and footer components with navigation highlighting, scroll effects, and smooth scrolling for anchor links.

--- js/components.js
+++ js/components.js
@@ -48,7 +48,7 @@ const footerTemplate = `
         </div>
     </div>
     <div class="footer-bottom">
-        <p>&copy; 2026 Maurya Global Export Company. All rights reserved.</p>
+        <p>&copy; 2026 Maurya Global Export Company. All rights reserved. Design by <a href="https://shivo1211.me/" target="_blank" style="color: inherit; text-decoration: none;">SHIVAM GUPTA</a></p>
     </div>
 </footer>
 `;
